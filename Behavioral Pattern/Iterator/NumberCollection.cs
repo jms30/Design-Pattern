f790b0261e0615ad78da9dd35f27573e71048c0f
Jay Shah <jayshah30392@gmail.com>
2020-01-14 23:06:47 -0500

    Initial Commit

---
+++ Behavioral Pattern/Iterator/NumberCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    /// <summary>
+    /// A collection class that holds the data internally 
+    /// and allows client to add/get the collected data.
+    /// It implements <see cref="IEnumerable{T}"/> interface that 
+    /// inherently allows iterating over the collection.
+    /// </summary>
+    public class NumberCollection : IEnumerable<int>
+    {
+        private List<int> m_InternalCollection = null;
+
+        private bool m_IsReversed = false;
+
+        public NumberCollection()
+        {
+            m_InternalCollection = new List<int>();
+        }
+
+        public IReadOnlyCollection<int> GetItems()
+        {
+            return m_InternalCollection.AsReadOnly();
+        }
+
+        public void ReverseDirection()
+        {
+            m_IsReversed = !m_IsReversed;
+        }
+
+        public void AddItem( int item )
+        {
+            m_InternalCollection.Add( item );
+        }
+
+        private IEnumerator GetEnumeratorPrivate()
+        {
+            return this.GetEnumerator();
+        }
+
+        #region IEnumerable Implementation
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new NumberCollectionIterator( m_InternalCollection, m_IsReversed );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumeratorPrivate();
+        }
+        #endregion
+    }
+}
---
+++ Behavioral Pattern/Iterator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            NumberCollection collection = new NumberCollection();
+            collection.AddItem( 5 );
+            collection.AddItem( 15 );
+            collection.AddItem( 25 );
+            collection.AddItem( 35 );
+            collection.AddItem( 45 );
+
+            Console.WriteLine( "Invoking forward directional Iterator..." );
+            foreach ( var item in collection )
+                Console.Write( " -> " + item );
+
+            collection.ReverseDirection();
+
+            Console.WriteLine( "\nInvoking reverse directional Iterator..." );
+            foreach ( var item in collection )
+                Console.Write( " -> " + item );
+        }
+    }
+}
